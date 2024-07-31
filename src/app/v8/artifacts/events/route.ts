22dc64e696d97e42b3cc6acaf56103508c0a543f
Lucas Bogerd <lucas@youngones.works>
2024-07-31 21:10:14 +0200

    chore: Add and run prettier

--- src/app/v8/artifacts/events/route.ts
+++ src/app/v8/artifacts/events/route.ts
@@ -5,13 +5,13 @@ import z from "zod";
 import { parseHeaders, parseQuery } from "~/utils/parsers";
 
 const headerSchema = z.object({
-  "x-artifact-client-ci": z.string(),
-  "x-artifact-client-interactive": z.union([z.literal("0"), z.literal("1")]),
+	"x-artifact-client-ci": z.string(),
+	"x-artifact-client-interactive": z.union([z.literal("0"), z.literal("1")]),
 });
 
 const querySchema = z.object({
-  teamId: z.string(),
-  slug: z.string(),
+	teamId: z.string(),
+	slug: z.string(),
 });
 
 const sourceSchema = z.enum(["LOCAL", "REMOTE"]);
@@ -19,45 +19,45 @@ const hitLiteral = z.literal("HIT");
 const missLiteral = z.literal("MISS");
 
 const baseBodySchema = z.object({
-  sessionId: z.string(),
-  source: sourceSchema,
-  hash: z.string().uuid(),
+	sessionId: z.string(),
+	source: sourceSchema,
+	hash: z.string().uuid(),
 });
 
 const bodySchema = z.array(
-  z.union([
-    baseBodySchema
-      .merge(
-        z.object({
-          event: missLiteral,
-        })
-      )
-      .strict(),
-    baseBodySchema
-      .merge(
-        z.object({
-          event: hitLiteral,
-          duration: z.number().positive(),
-        })
-      )
-      .strict(),
-  ])
+	z.union([
+		baseBodySchema
+			.merge(
+				z.object({
+					event: missLiteral,
+				}),
+			)
+			.strict(),
+		baseBodySchema
+			.merge(
+				z.object({
+					event: hitLiteral,
+					duration: z.number().positive(),
+				}),
+			)
+			.strict(),
+	]),
 );
 
 export async function POST(request: NextRequest) {
-  try {
-    const headers = parseHeaders(headerSchema)(request.headers);
+	try {
+		const headers = parseHeaders(headerSchema)(request.headers);
 
-    // get query params from the request
-    const searchParams = request.nextUrl.searchParams;
-    const query = parseQuery(querySchema)(searchParams);
+		// get query params from the request
+		const searchParams = request.nextUrl.searchParams;
+		const query = parseQuery(querySchema)(searchParams);
 
-    const body = bodySchema.parse(await request.json());
+		const body = bodySchema.parse(await request.json());
 
-    console.log({ headers, query, body });
+		console.log({ headers, query, body });
 
-    return new Response("OK", { status: 200 });
-  } catch (error) {
-    return new Response((error as Object).toString(), { status: 400 });
-  }
+		return new Response("OK", { status: 200 });
+	} catch (error) {
+		return new Response((error as Object).toString(), { status: 400 });
+	}
 }
--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,17 +1,17 @@
 import { z, ZodObject, ZodSchema } from "zod";
 
 export function parseHeaders<T extends z.ZodType<object>>(schema: T) {
-  return (headers: Headers): z.infer<T> => {
-    const parsedHeaders = Object.fromEntries(headers.entries());
+	return (headers: Headers): z.infer<T> => {
+		const parsedHeaders = Object.fromEntries(headers.entries());
 
-    return schema.parse(parsedHeaders);
-  };
+		return schema.parse(parsedHeaders);
+	};
 }
 
 export function parseQuery<T extends z.ZodType<object>>(schema: T) {
-  return (searchParams: URLSearchParams): z.infer<T> => {
-    const parsedQuery = Object.fromEntries(searchParams.entries());
+	return (searchParams: URLSearchParams): z.infer<T> => {
+		const parsedQuery = Object.fromEntries(searchParams.entries());
 
-    return schema.parse(parsedQuery);
-  };
+		return schema.parse(parsedQuery);
+	};
 }
