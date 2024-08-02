269e0f5c745d9c61f685cd263dcd28269b7e45ef
Lucas Bogerd <lucas@youngones.works>
2024-08-02 23:50:26 +0200

    refactor: Make file upload path  an env var

--- src/app/v8/artifacts/[hash]/route.ts
+++ src/app/v8/artifacts/[hash]/route.ts
@@ -2,6 +2,7 @@ export const dynamic = "force-dynamic";
 
 import { NextRequest } from "next/server";
 import { promises as fs } from "fs";
+import { env } from "~/utils/env.mjs";
 
 export async function PUT(
 	request: NextRequest,
@@ -15,16 +16,16 @@ export async function PUT(
 
 		// check if directory exists, if not create it
 		try {
-			await fs.access("./uploads");
+			await fs.access(env.UPLOAD_PATH);
 		} catch (error) {
-			await fs.mkdir("./uploads");
+			await fs.mkdir(env.UPLOAD_PATH);
 		}
 
 		// get the file from the request and save it to the file system
 		const file = await request.blob();
 		const arrayBuffer = await file.arrayBuffer();
 
-		await fs.writeFile(`./uploads/${hash}`, Buffer.from(arrayBuffer));
+		await fs.writeFile(`${env.UPLOAD_PATH}/${hash}`, Buffer.from(arrayBuffer));
 
 		return new Response(null, { status: 202 });
 	} catch (error) {
@@ -43,7 +44,7 @@ export async function GET(
 			throw new Error("No hash provided");
 		}
 
-		const file = await fs.readFile(`./uploads/${hash}`);
+		const file = await fs.readFile(`${env.UPLOAD_PATH}/${hash}`);
 
 		return new Response(file, { status: 200 });
 	} catch (error) {
@@ -63,7 +64,7 @@ export async function HEAD(
 		}
 
 		try {
-			await fs.access(`./uploads/${hash}`);
+			await fs.access(`${env.UPLOAD_PATH}/${hash}`);
 		} catch (error) {
 			return new Response(null, { status: 404 });
 		}
--- src/utils/env.mjs
+++ src/utils/env.mjs
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
 	server: {
 		DB_URL: z.string().url(),
+		UPLOAD_PATH: z.string(),
 	},
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
