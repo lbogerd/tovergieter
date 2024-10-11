a149baf73be68a6ce0150fffce1f8bcb30901b7e
Lucas Bogerd <lucas@youngones.works>
2024-10-11 17:16:02 +0200

    fix: Enable and use top-level await for db

--- src/utils/db/db.ts
+++ src/utils/db/db.ts
@@ -2,4 +2,4 @@ import { drizzle } from "drizzle-orm/connect";
 import { env } from "../env.mjs";
 
 const dbClient = env.NODE_ENV === "production" ? "neon-http" : "postgres-js";
-export const db = drizzle(dbClient, env.DB_URL);
+export const db = await drizzle(dbClient, env.DB_URL);
