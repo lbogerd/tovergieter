9f669cb415cb31281c7226ecd8d6f7ece5b864a1
Lucas Bogerd <lucas.bogerd@basic-fit.com>
2024-10-15 16:19:07 +0200

    fix: Use standalone output for use in Docker

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const ensureEnvIsValidated = env;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ["@node-rs/argon2"],
-	},
+	output: "standalone",
 };
 
 export default nextConfig;
